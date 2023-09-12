36b1c969608a1389ee4922a09a728091686d86a9
Aritro Saha <titanssuperior@gmail.com>
2023-09-12 19:54:33 +0530

    "Changes in Responsiveness"

--- Client/src/components/MoviePage.jsx
+++ Client/src/components/MoviePage.jsx
@@ -6,6 +6,7 @@ function MoviePage(props) {
 
   const { data } = props;
   const [isInputFocused, setIsInputFocused] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   const aboutRef = useRef(null);
 
   const handleClick = () => {
@@ -28,12 +29,22 @@ function MoviePage(props) {
   };
 
   useEffect(() => {
-    // Add a click event listener to handle clicks outside the "about" div
     document.addEventListener("click", handleClickOutside);
 
     return () => {
-      // Remove the click event listener when the component unmounts
       document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -42,7 +53,7 @@ function MoviePage(props) {
     <div className={"about "+(data&&isInputFocused?"active":"")} ref={aboutRef}>
       <div className="fit">
       <svg onClick={handleClick} stroke="currentColor" fill="currentColor" strokeWidth="0" version="1.1" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M15.854 12.854c-0-0-0-0-0-0l-4.854-4.854 4.854-4.854c0-0 0-0 0-0 0.052-0.052 0.090-0.113 0.114-0.178 0.066-0.178 0.028-0.386-0.114-0.529l-2.293-2.293c-0.143-0.143-0.351-0.181-0.529-0.114-0.065 0.024-0.126 0.062-0.178 0.114 0 0-0 0-0 0l-4.854 4.854-4.854-4.854c-0-0-0-0-0-0-0.052-0.052-0.113-0.090-0.178-0.114-0.178-0.066-0.386-0.029-0.529 0.114l-2.293 2.293c-0.143 0.143-0.181 0.351-0.114 0.529 0.024 0.065 0.062 0.126 0.114 0.178 0 0 0 0 0 0l4.854 4.854-4.854 4.854c-0 0-0 0-0 0-0.052 0.052-0.090 0.113-0.114 0.178-0.066 0.178-0.029 0.386 0.114 0.529l2.293 2.293c0.143 0.143 0.351 0.181 0.529 0.114 0.065-0.024 0.126-0.062 0.178-0.114 0-0 0-0 0-0l4.854-4.854 4.854 4.854c0 0 0 0 0 0 0.052 0.052 0.113 0.090 0.178 0.114 0.178 0.066 0.386 0.029 0.529-0.114l2.293-2.293c0.143-0.143 0.181-0.351 0.114-0.529-0.024-0.065-0.062-0.126-0.114-0.178z"></path></svg>
-        <img src={data&&data.backdrop} alt={data&&data.movie_name} />
+        <img src={screenWidth < 800 ? (data && data.poster) : (data && data.backdrop)} alt={data&&data.movie_name} />
         <div className="cover">
           <h1>{data&&data.movie_name}</h1>
           <h2>Release Date: {data&&data.releasedate}</h2>
